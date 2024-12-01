5edc37f4437e34b8922cb50ab717b0d9a5470220
aicha652 <aichaazlf652@gmail.com>
2024-12-01 13:51:56 +0100

    Typeahead

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Typeahead } from 'react-bootstrap-typeahead'; 
+import 'react-bootstrap-typeahead/css/Typeahead.css';
 
 function App() {
+  const [singleSelections, setSingleSelections] = useState([]);
+
+  const options = [
+    {id: 1, name: "JavaScript"},
+    {id: 2, name: "Java"},
+    {id: 3, name: "Html"},
+    {id: 4, name: "CSS"},
+    {id: 5, name: "Tailwind"},
+    {id: 6, name: "Bootstrap"},
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='form-group'>
+      <Typeahead 
+         id="basic-typeahead-single"
+         onChange={setSingleSelections}
+         labelKey="name"
+         options={options}
+         selected={singleSelections}
+         placeholder="Choose a technology..."
+         renderMenuItemChildren={(option) => (
+          <div style={{ display: 'block', marginBottom: '10px', textDecoration: 'none' }}>
+            {option.name}
+          </div>
+        )} />
     </div>
   );
 }
